2a45dbf2da7154df31215150a5fb37a8d843505c
nehaupadhye <79204762+nehaupadhye@users.noreply.github.com>
2022-03-03 13:07:32 +0530

    Add files via upload

---
+++ check-eligibility.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-eligibility',
+  templateUrl: './check-eligibility.component.html',
+  styleUrls: ['./check-eligibility.component.scss']
+})
+export class CheckEligibilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
